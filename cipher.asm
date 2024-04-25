7bde3fdb061c9fb9339146d4ec447220b1780b9d
beatrizmontenegro07 <beatriz.m.maia07@gmail.com>
2024-04-25 16:48:09 -0300

    leitura/escrita de arquivos feito

--- cipher.asm
+++ cipher.asm
@@ -5,17 +5,26 @@ option casemap:none
 include \masm32\include\windows.inc
 include \masm32\include\kernel32.inc
 include \masm32\include\masm32.inc
+include \masm32\include\msvcrt.inc
 includelib \masm32\lib\kernel32.lib
 includelib \masm32\lib\masm32.lib
+includelib \masm32\lib\msvcrt.lib ;LEMBRAR DE TIRAR DEPOIS
+include \masm32\macros\macros.asm ;LEMBRAR DE TIRAR DEPOIS
+
+
+.data?
+readCount dd ?
+writeCount dd ?
 
 .data
 
-;STRING DO MENU (menssagem do menu e da entrada de dado)
-menuOutput db "[1] Criptografar", 13, 10, "[2] Descriptografar", 13, 10,"[3] Sair",13, 10,"Informe sua escolha: ",0h
+;VARTIAVEIS DE MENU (menssagem do menu e entrada com a escolha do usuario)
+menuOutput db 13, 10, "------- MENU -------", 13, 10, "[1] Criptografar", 13, 10, "[2] Descriptografar", 13, 10,"[3] Sair",13, 10,"Informe sua escolha: ",0h
+inputEscolha db 16 dup(0)
 
 ;VARIAVEIS DE ENTRADA
-inputEscolha db 16 dup(0)
 escolha dd 0
+chaveArray dword 8 dup(0)
 
 ;VARIAVEIS DE PRINT/INPUT
 outputHandle dd 0
@@ -23,15 +32,22 @@ inputHandle dd 0
 console_count dd 0
 
 ;STRING DE ENCERRAMENTO
-finalOutput db "Programa encerrado!", 0h
+finalOutput db 13, 10, "Programa encerrado!", 0h
 
-;STRING DE ARQUIVO
-outputArq1 db "Digite o arquivo de entrada: ", 0ah, 0h
-outputArq2 db "Digite o nome do arquivo de saida(criptografado): ", 0ah, 0h
-outputChave db "Digite a chave de criptografia: ", 0ah, 0h 
-inputArqEntrada db 20 dup(0)
-inputChave db 10 dup(0)
-inputArqSaida db 20 dup (0)
+;OUTPUT DE ARQUIVO
+outputArq1 db 13, 10, "Digite o arquivo de entrada (max de 50 caracteres): ", 0h
+outputArq2 db "Digite o nome do arquivo de saida(max de 50 caracteres): ", 0h
+outputChave db "Digite a chave (max 8 digitos, 0-7): ", 0h 
+
+;INPUT DE ARQUIVO/CHAVE
+inputArqEntrada db 50 dup(0)
+inputArqSaida db 50 dup (0)
+inputChave db 8 dup(0)
+
+;VARIAVEIS DE ARQUIVO
+fileHandleEntrada dd 0
+fileHandleSaida dd 0
+bufferEntrada db 8 dup(0)
 
 
 .code
@@ -41,19 +57,20 @@ start:
         invoke GetStdHandle, STD_OUTPUT_HANDLE
         mov outputHandle, eax
         invoke WriteConsole, outputHandle, addr menuOutput, sizeof menuOutput, addr console_count, NULL
+        
         ;Entrada de dado da opção escolhida pelo usuário
         invoke GetStdHandle, STD_INPUT_HANDLE
         mov inputHandle, eax
         invoke ReadConsole, inputHandle, addr inputEscolha, sizeof inputEscolha, addr console_count, NULL
         invoke StrLen, addr inputEscolha
 
-        ;Tratamento de entrada
+        ;Tratamento de entrada do usuário
         mov esi, offset inputEscolha
-        proximo:
+        proximoMenu:
             mov al, [esi]
             inc esi
             cmp al, 13
-            jne proximo
+            jne proximoMenu
             dec esi
             xor al, al
             mov [esi], al
@@ -62,21 +79,26 @@ start:
         invoke atodw, addr inputEscolha
         mov escolha, eax
 
+        ;Caso o usuário tenha optado por criptografia
         cmp escolha, 1
-        je criptografar
-
+        je lerArquivosChave
+        
+        ;Caso o usuário tenha optado por descriptografia
         cmp escolha, 2
-        je descriptografar
-
+        je lerArquivosChave
+        
+        ;Caso o usuário tenha optado por sair
         cmp escolha, 3
         je fim
 
-    criptografar:
-        ;aqui vai toda a logica de criptografia
+        ;caso nao tenha sido digitado um numero valido pelo usuario
+        jmp menu
+
+    ;leitura de arquivos de entrada/saida e a chave para realizar a criptografia
+    lerArquivosChave:
 
         ;perguntar o arquivo de entrada
-        push STD_OUTPUT_HANDLE
-        call GetStdHandle
+        invoke GetStdHandle, STD_OUTPUT_HANDLE
         mov outputHandle, eax
         invoke WriteConsole, outputHandle, addr outputArq1, sizeof outputArq1 -1, addr console_count, NULL
         
@@ -86,9 +108,20 @@ start:
         invoke ReadConsole, inputHandle, addr inputArqEntrada, sizeof inputArqEntrada, addr console_count, NULL
         invoke StrLen, addr inputArqEntrada
 
+        ;tratamento do arquivo de entrada
+        mov esi, offset inputArqEntrada
+        proximoEntrada:
+            mov al, [esi]
+            inc esi
+            cmp al, 13
+            jne proximoEntrada
+        dec esi
+        xor al, al
+        mov [esi], al
+        
+
         ;perguntar o nome do arquivo de saida
-        push STD_OUTPUT_HANDLE
-        call GetStdHandle
+        invoke GetStdHandle, STD_OUTPUT_HANDLE
         mov outputHandle, eax
         invoke WriteConsole, outputHandle, addr outputArq2, sizeof outputArq2 -1, addr console_count, NULL
         
@@ -98,25 +131,108 @@ start:
         invoke ReadConsole, inputHandle, addr inputArqSaida, sizeof inputArqSaida, addr console_count, NULL
         invoke StrLen, addr inputArqSaida
 
-        ;perguntar a chave de criptografia
-        push STD_OUTPUT_HANDLE
-        call GetStdHandle
-        mov outputHandle, eax
-        invoke WriteConsole, outputHandle, addr outputChave, sizeof outputChave -1, addr console_count, NULL
+        ;tratamento do arquivo de saida
+        mov esi, offset inputArqSaida
+        proximoSaida:
+            mov al, [esi]
+            inc esi
+            cmp al, 13
+            jne proximoSaida
+            dec esi
+            xor al, al
+            mov [esi], al
         
-        ;receber o nome do arquivo de saida
+        ;perguntar a chave
+        invoke GetStdHandle, STD_OUTPUT_HANDLE
+        mov outputHandle, eax
+        invoke WriteConsole, outputHandle, addr outputChave, sizeof outputChave, addr console_count, NULL
+        
+        ;receber a chave 
         invoke GetStdHandle, STD_INPUT_HANDLE
         mov inputHandle, eax
         invoke ReadConsole, inputHandle, addr inputChave, sizeof inputChave, addr console_count, NULL
         invoke StrLen, addr inputChave
+
+        ;tratamento da chave
+        mov esi, offset inputChave
+        proximoChave:
+            mov al, [esi]
+            inc esi
+            cmp al, 13
+            jne proximoChave
+            dec esi
+            xor al, al
+            mov [esi], al
+
+        ;Convertendo a string para um array do tipo dword (CORRIGIR DEPOIS DANDO ERRO)
+        mov esi, offset inputChave ;aponta para o inicio do input da chave (string)
+        mov edi, offset chaveArray ;aponta para o inicio do vetor do tipo dword
+        mov ecx, 8
+        conversor:
+            xor eax, eax
+            mov al, [esi] ;move o caractere atual para al
+            sub al, '0' ;converte para o valor correspondente na tabela ascii
+            ;mov [edi], eax ;move o valor para uma posição no array dword chave
+            ;add edi, 4 ;proxima posição no array de dword 
+            inc esi ;proximo caractere na string
+            dec ecx
+            cmp ecx, 0 ;verifica se pecorreu todas as posicoes
+            jge conversor ;se não tiver pecorrido tudo, volta pro label conversor
         
+
+        ;abertura do arquivo de entrada
+        invoke CreateFile, addr inputArqEntrada, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL
+        mov fileHandleEntrada, eax
+
+        ;criar o arquivo de saida
+        invoke CreateFile, addr inputArqSaida, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL
+        mov fileHandleSaida, eax
+
+         
+        ;caso o usuário tenha escolhido 1, executa a criptografia
+        cmp escolha, 1
+        je criptografar
+
+        ;caso o usuário tenha escolhido 2, executa a descriptografia
+        cmp escolha, 2
+        je descriptografar
+
+    criptografar:
+        ;aqui vai toda a logica de criptografia
+        ;leitura de 8bytes
+        continuar:
+            mov esi, offset bufferEntrada
+            mov ecx, 0
+            ;zerar buffer de entrada
+            zerar:
+                mov al, [esi]
+                xor al, al
+                mov [esi], al
+                inc esi
+                inc ecx
+                cmp ecx, 8
+                jne zerar
+                    
+            invoke ReadFile, fileHandleEntrada, addr bufferEntrada, 8, addr readCount, NULL
+            cmp readCount, 0
+            je finalizar
+            ;escrita no arquivo de saida 
+            invoke WriteFile, fileHandleSaida, addr bufferEntrada, 8, addr writeCount, NULL
+            jmp continuar
+            
+        finalizar:
+        ;fechar o arquivo
+        invoke CloseHandle, fileHandleEntrada
+        invoke CloseHandle, fileHandleSaida
+                
         jmp menu
 
     descriptografar:
-        ;aqui vai toda a logica de criptografia
+        ;aqui vai toda a logica de descriptografia
         jmp menu
            
     fim:
+        ;menssagem e encerramento do programa
         invoke GetStdHandle, STD_OUTPUT_HANDLE
         mov outputHandle, eax
         invoke WriteConsole, outputHandle, addr finalOutput, sizeof finalOutput, addr console_count, NULL
